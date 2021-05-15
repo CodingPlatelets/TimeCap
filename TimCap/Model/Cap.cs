73a99deacfd87e3d46bc2884049695afda21e32a
WenkanEdward <wenkanedward@outlook.com>
2021-05-15 12:08:52 +0800

    models

---
+++ TimCap/Model/Cap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimCap.Model
+{
+    public class Cap
+    {
+        public int Id { get; set; }
+        public string Story { get; set; }
+        public DateTime Time { get; set; }
+        public string Address { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
