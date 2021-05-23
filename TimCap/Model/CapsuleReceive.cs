2fc5abe828a2c6dd5212a1f74a55b6635cfdc7fd
WenkanEdward <wenkanedward@outlook.com>
2021-05-23 14:53:59 +0800

    修改故事限制长度

--- TimCap/Model/CapsuleReceive.cs
+++ TimCap/Model/CapsuleReceive.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +15,7 @@ namespace TimCap.Model
         /// <summary>
         /// 故事
         /// </summary>
+        [MaxLength(800)]
         public string Story { get; set; }
     }
 }
